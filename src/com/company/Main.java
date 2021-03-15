973758d76592547c60988235981e99ee20c48624
Malik Robinson <malikrob263@yahoo.com>
2021-03-14 20:29:08 -0400

    My Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World 2!");
+        GameItem sample = new GameItem("Hack 'n Slash",GameSize.M, 59.99,1);
+
+        var output = String.format("Your %s, %s game will cost %s.", sample.getSize(), sample.getType(), sample.getPrice());
+        System.out.println(output);
     }
 }
