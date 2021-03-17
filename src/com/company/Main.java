646c75c329c87bfba2ff50b3ef31481988eb1f68
Malik Robinson <malikrob263@yahoo.com>
2021-03-16 22:09:18 -0400

    My changes
    Properly commented the Main file for this program.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,32 @@ import com.company.model.HnS;
 
 import java.text.NumberFormat;
 
+/**
+ * @author malik
+ *
+ * This program will display the details of an array of objects from the GameItem class. While an enum and two
+ * sub-classes will help with streamlining the construction of said objects.
+ *
+ * The array of objects will be displayed by a for-each loop that calls a method that will display the qualities of the
+ * objects in an ordely fashion, one line per object.
+ *
+ * @version 1.0
+ * @since 03/16/2021
+ */
 public class Main {
 
+    /**
+     *
+     * @param args
+     *
+     * This method holds the called array of GameItem; each constructed with different values in each category.
+     * A for-each loop will iterate through the array and call a method for each object within that will display the
+     * objects information in an orderly fashion.
+     * No user input required; all automatic.
+     */
     public static void main(String[] args) {
 
-        /*GameItem itemA = new HnS(GameSize.M, 29.99, 2);
-        displayGameItemDetail(itemA);
-
-        GameItem itemB = new FPS(GameSize.L, 59.99, 1);
-        displayGameItemDetail(itemB);*/
-
+        //Array of GameItem to hold the two objects.
         GameItem[] items = {
                 new HnS(
                         GameSize.M,
@@ -28,17 +44,33 @@ public class Main {
                         1)
         };
 
+        //For-each loop that calls the method for displaying the information of each object.
         for (GameItem item: items) {
             displayGameItemDetail(item);
         }
     }
 
+    /**
+     *
+     * @param item Requires an instance of GameItem for the method calls within to work.
+     *
+     * This method gets the total price of the purchase by multiplying the individual price and quantity, ensures the
+     * result is in the proper format by calling NumberFormat.getCurrencyInstance, and formats the string so that all
+     * information can be displayed in the console in an orderly fashion.
+     */
     private static void displayGameItemDetail (GameItem item) {
+
+        //Getting a proper total of expense based on base price times quantity.
         var totalPrice = item.getPrice() * item.getQuantity();
+
+        //Declares and initializes a formatter for use in the String.format.
         var formatter = NumberFormat.getCurrencyInstance();
-        var output = String.format("Your %s %s purchase will cost %s. Quantity: %s",
+
+        //Uses %s to place the information called via methods at the right parts in the string.
+        var output = String.format("Your %s, %s purchase will cost %s. Quantity: %s",
                 item.getSize(),
                 item.getType(),
+                //Gives total price the right format for currency.
                 formatter.format(totalPrice),
                 item.getQuantity());
         System.out.println(output);
